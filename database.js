afc95bff62280e4b503812294d650133df4f14e5
Jon Dough <jetstream0.7m@gmail.com>
2021-03-04 19:40:08 +0000

    added functions in db, incomplete

--- database.js
+++ database.js
@@ -1,7 +1,28 @@
 //https://firebase.google.com/docs/admin/setup
+//https://firebase.google.com/docs/database/admin/start#node.js
+//https://firebase.google.com/docs/database/admin/save-data
+//https://firebase.google.com/docs/database/admin/retrieve-data
 const admin = require('firebase-admin');
 
 //we need to set up a firebase db and add credential here
 admin.initializeApp();
 
-const db = admin.firestore();
+const db = admin.database();
+
+//add is the same as set
+function add(path, document) {
+
+}
+
+function update(path, document) {
+
+}
+
+function push(path, document) {
+
+}
+
+//read is getting 
+function read(path, key) {
+
+}
