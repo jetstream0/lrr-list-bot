66496f41e5418ee26e8681e03436949834988fd2
Jon Dough <jetstream0.7m@gmail.com>
2021-03-04 21:59:46 +0000

    db initializing

--- database.js
+++ database.js
@@ -2,10 +2,19 @@
 //https://firebase.google.com/docs/database/admin/start#node.js
 //https://firebase.google.com/docs/database/admin/save-data
 //https://firebase.google.com/docs/database/admin/retrieve-data
+//https://github.com/rk097/gdlrrlist.cf/blob/main/htdocs/JS/mainlist.json
+//https://github.com/rk097/gdlrrlist.cf/blob/main/htdocs/JS/leaderboard.json
 const admin = require('firebase-admin');
 
+const serviceAccount = require('./service-account-file.json');
+
+const {dbURL} = process.env;
+
 //we need to set up a firebase db and add credential here
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: dbURL
+});
 
 const db = admin.database();
 
